bb2971aaba88bf07da59448ed2d54b363c2bd344
Ariana Davis <arianadavisj@gmail.com>
2023-01-04 18:56:07 -0500

    updating

--- schema.sql
+++ schema.sql
@@ -34,9 +34,12 @@ CREATE TABLE salaries (
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no)
 );
+
 CREATE TABLE titles (
 	emp_no INT NOT NULL, 
 	title VARCHAR NOT NULL,
